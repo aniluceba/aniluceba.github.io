350ff9bd2acd76d5e7829d028b8bd5e9f9b1118c
Ana Lucía Ceballos Cáceres <106625583+aniluceba@users.noreply.github.com>
2023-02-28 21:33:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,24 +12,3 @@ $(document).ready(function(){
 
     });
 
-
-//scrolling
-
-$('a[href*="#"]').on('click',function(e){
-
-    e.preventDefault();
-
-    $('html, body').animate({
-
-        scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      500,
-      'linear'
-    });
-
-});
-
-});
-
-
